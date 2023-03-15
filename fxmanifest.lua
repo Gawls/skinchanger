3160751dc4541e01d43e5a03a5697aa1e53bb4b3
Gawls <matt.gawley@gmail.com>
2023-03-14 23:58:52 -0600

    Update and rename __resource.lua to fxmanifest.lua
    Convert from ESX to QBCore

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,7 @@
+fx_version "cerulean"
+
+game "gta5"
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Skin Changer'
@@ -14,3 +18,5 @@ client_scripts {
 	'config.lua',
 	'client/main.lua'
 }
+
+lua54 'yes'
